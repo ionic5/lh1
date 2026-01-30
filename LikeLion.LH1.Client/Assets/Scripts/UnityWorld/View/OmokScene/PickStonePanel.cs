7d88d3ad6a956d5b54ad5f31c80b5973fdd29de2
ionic5 <st.kaldalis@gmail.com>
2026-01-30 21:18:08 +0900

    PickStonePanel 구현

---
+++ LikeLion.LH1.Client/Assets/Scripts/UnityWorld/View/OmokScene/PickStonePanel.cs
@@ -0,0 +1,27 @@
+﻿using LikeLion.LH1.Client.Core.View.OmokScene;
+using System;
+using UnityEngine;
+
+namespace LikeLion.LH1.Client.UnityWorld.View.OmokScene
+{
+    public class PickStonePanel : MonoBehaviour, IPickStonePanel
+    {
+        public event EventHandler WhiteStoneButtonClickedEvent;
+        public event EventHandler BlackStoneButtonClickedEvent;
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnWhiteStoneButtonClicked()
+        {
+            WhiteStoneButtonClickedEvent?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void OnBlackStoneButtonClicked()
+        {
+            BlackStoneButtonClickedEvent?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
