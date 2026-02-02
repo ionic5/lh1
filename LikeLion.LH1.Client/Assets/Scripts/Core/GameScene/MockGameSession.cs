2c117b56ac32784d2a6485c62392691355930a9b
ionic5 <st.kaldalis@gmail.com>
2026-02-02 17:36:03 +0900

    MockGameSession 일부 구현

--- LikeLion.LH1.Client/Assets/Scripts/Core/GameScene/MockGameSession.cs
+++ LikeLion.LH1.Client/Assets/Scripts/Core/GameScene/MockGameSession.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LikeLion.LH1.Client.Core.GameScene
 {
@@ -11,29 +13,86 @@ namespace LikeLion.LH1.Client.Core.GameScene
         public event EventHandler<PlayerTurnFinishedEventArgs> PlayerTurnFinishedEvent;
         public event EventHandler<GameFinishedEventArgs> GameFinishedEvent;
 
+        private List<Player> _players;
+
+        private Core.Timer _timer;
+
+        private class Player
+        {
+            public string PlayerGuid;
+            public int StoneType;
+        }
+
+        public MockGameSession()
+        {
+            _players = new List<Player>();
+        }
+
         public void PickStone(string gameGuid, string playerGuid, int stoneType)
         {
-            throw new NotImplementedException();
+            var player = _players.First(entry => entry.PlayerGuid == playerGuid);
+            player.StoneType = stoneType;
+
+            var otherPlayer = _players.First(entry => entry.PlayerGuid != playerGuid);
+            otherPlayer.StoneType = StoneType.Black == stoneType ? StoneType.White : StoneType.Black;
+
+            GamePreparedEvent?.Invoke(this, EventArgs.Empty);
         }
 
         public void PutStone(string gameGuid, string playerGuid, int column, int row)
         {
-            throw new NotImplementedException();
+            var stoneType = _players.Where(entry => entry.PlayerGuid == playerGuid).Select(entry => entry.StoneType).First();
+
+            _timer.Stop(0);
+
+            PlayerTurnFinishedEvent?.Invoke(this, new PlayerTurnFinishedEventArgs
+            {
+                PlayerGuid = playerGuid,
+                StoneType = stoneType,
+                Column = column,
+                Row = row
+            });
+
+            var otherPlayerGuid = _players.Where(entry => entry.PlayerGuid != playerGuid).Select(entry => entry.PlayerGuid).First();
+            StartTurn(otherPlayerGuid);
         }
 
         public void RequestConnect()
         {
-            throw new NotImplementedException();
+            var mainPlayer = new Player { PlayerGuid = Guid.NewGuid().ToString(), StoneType = StoneType.Null };
+            _players.Add(mainPlayer);
+
+            ConnectedEvent?.Invoke(this, new ConnectedEventArgs { PlayerGuid = mainPlayer.PlayerGuid });
         }
 
         public void RequestGame(string playerGuid)
         {
-            throw new NotImplementedException();
+            _players.Add(new Player { PlayerGuid = Guid.NewGuid().ToString(), StoneType = StoneType.Null });
+
+            GameCreatedEvent?.Invoke(this, new GameCreatedEventArgs { GameGuid = Guid.NewGuid().ToString() });
         }
 
         public void StartGame(string gameGuid, string playerGuid)
         {
-            throw new NotImplementedException();
+            var player = _players.First(entry => entry.StoneType == StoneType.Black);
+
+            StartTurn(player.PlayerGuid);
+        }
+
+        private void StartTurn(string playerGuid)
+        {
+            PlayerTurnStartedEvent?.Invoke(this, new PlayerTurnStartedEventArgs { PlayerGuid = playerGuid });
+
+            _timer.Start(0, 60, () =>
+            {
+                PlayerTurnFinishedEvent?.Invoke(this, new PlayerTurnFinishedEventArgs
+                {
+                    PlayerGuid = playerGuid,
+                    StoneType = StoneType.Null,
+                    Column = -1,
+                    Row = -1
+                });
+            });
         }
     }
 }
