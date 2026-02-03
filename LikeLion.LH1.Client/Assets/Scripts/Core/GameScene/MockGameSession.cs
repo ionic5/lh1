20a0ebed560c601721a5f8c78d8bb872a8fdfc2c
ionic5 <st.kaldalis@gmail.com>
2026-02-03 17:19:44 +0900

    MockGameSession 정리

--- LikeLion.LH1.Client/Assets/Scripts/Core/GameScene/MockGameSession.cs
+++ LikeLion.LH1.Client/Assets/Scripts/Core/GameScene/MockGameSession.cs
@@ -14,74 +14,58 @@ namespace LikeLion.LH1.Client.Core.GameScene
         public event EventHandler<PlayerTurnFinishedEventArgs> PlayerTurnFinishedEvent;
         public event EventHandler<GameFinishedEventArgs> GameFinishedEvent;
 
-        private IAIConsole _aiConsole;
         private Core.Timer _timer;
-        private string _gameGuid;
-        private List<Player> _players;
-        private List<List<int>> _board;
         private float _timeLimit;
-
-        private class Player
-        {
-            public string PlayerGuid;
-            public int StoneType;
-        }
+        private Entity.Checkerboard _checkerboard;
+        private IPlayer _dummyPlayer;
 
         public MockGameSession(IAIConsole aiConsole, Timer timer)
         {
             _timeLimit = 8;
-            _players = new List<Player>();
-            _aiConsole = aiConsole;
             _timer = timer;
-
-            _board = new List<List<int>>();
-            for (int i = 0; i < 19; i++)
-            {
-                List<int> row = new List<int>();
-                for (int j = 0; j < 19; j++)
-                    row.Add(StoneType.Null);
-                _board.Add(row);
-            }
         }
 
         public void RequestConnect()
         {
-            var mainPlayer = new Player { PlayerGuid = Guid.NewGuid().ToString(), StoneType = StoneType.Null };
-            _players.Add(mainPlayer);
+            var playerGuid = Guid.NewGuid().ToString();
 
-            ConnectedEvent?.Invoke(this, new ConnectedEventArgs { PlayerGuid = mainPlayer.PlayerGuid });
+            ConnectedEvent?.Invoke(this, new ConnectedEventArgs { PlayerGuid = playerGuid });
         }
 
         public void RequestGame(string playerGuid)
         {
-            _players.Add(new Player { PlayerGuid = Guid.NewGuid().ToString(), StoneType = StoneType.Null });
+            var dummyPlayerGuid = Guid.NewGuid().ToString();
+            _dummyPlayer.SetPlayerGuid(dummyPlayerGuid);
 
-            GameCreatedEvent?.Invoke(this, new GameCreatedEventArgs { GameGuid = Guid.NewGuid().ToString() });
+            var gameGuid = Guid.NewGuid().ToString();
+            _checkerboard.SetGameGuid(gameGuid);
+            _checkerboard.Setup();
+
+            GameCreatedEvent?.Invoke(this, new GameCreatedEventArgs { GameGuid = gameGuid });
         }
 
         public void PickStone(string gameGuid, string playerGuid, int stoneType)
         {
-            var player = _players.First(entry => entry.PlayerGuid == playerGuid);
-            player.StoneType = stoneType;
+            _checkerboard.RegisterStoneOwner(playerGuid, stoneType);
 
-            var otherPlayer = _players.First(entry => entry.PlayerGuid != playerGuid);
-            otherPlayer.StoneType = StoneType.Black == stoneType ? StoneType.White : StoneType.Black;
+            var dummyPlayerStoneType = StoneType.Black == stoneType ? StoneType.White : StoneType.Black;
+            _checkerboard.RegisterStoneOwner(_dummyPlayer.GetPlayerGuid(), dummyPlayerStoneType);
 
             var stoneOwners = new List<StoneOwner>
             {
-                new StoneOwner { PlayerGuid = player.PlayerGuid, StoneType = player.StoneType },
-                new StoneOwner { PlayerGuid = otherPlayer.PlayerGuid, StoneType = otherPlayer.StoneType }
+                new StoneOwner { PlayerGuid = playerGuid, StoneType = stoneType },
+                new StoneOwner { PlayerGuid = _dummyPlayer.GetPlayerGuid(), StoneType = dummyPlayerStoneType }
             };
             GamePreparedEvent?.Invoke(this, new GamePreparedEventArgs { StoneOwners = stoneOwners });
         }
 
         public void StartGame(string gameGuid, string playerGuid)
         {
-            var player = _players.First(entry => entry.StoneType == StoneType.Black);
-
             PlayerTurnStartedEvent += OnPlayerTurnStartedEvent;
+            PlayerTurnFinishedEvent += OnPlayerTurnFinishedEvent;
 
-            StartTurn(player.PlayerGuid);
+            var firstTurnPlayerGuid = _checkerboard.GetPlayerGuid(StoneType.Black);
+            StartTurn(firstTurnPlayerGuid);
         }
 
         private void StartTurn(string playerGuid)
@@ -102,16 +86,16 @@ namespace LikeLion.LH1.Client.Core.GameScene
                     Row = -1
                 });
 
-                var otherPlayerGuid = _players.Where(entry => entry.PlayerGuid != playerGuid).Select(entry => entry.PlayerGuid).First();
-                StartTurn(otherPlayerGuid);
+                var opponentPlayerGuid = _checkerboard.GetOpponentPlayerGuid(playerGuid);
+                StartTurn(opponentPlayerGuid);
             });
         }
 
         public void PutStone(string gameGuid, string playerGuid, int column, int row)
         {
-            var stoneType = _players.Where(entry => entry.PlayerGuid == playerGuid).Select(entry => entry.StoneType).First();
+            var stoneType = _checkerboard.GetStone(playerGuid);
 
-            _board[column][row] = stoneType;
+            _checkerboard.PutStone(column, row, stoneType);
 
             _timer.Stop(0);
 
@@ -123,99 +107,42 @@ namespace LikeLion.LH1.Client.Core.GameScene
                 Row = row
             });
 
-            var winnerStone = CheckWinner(_board.Select(row => row.ToArray()).ToArray());
+            var winnerStone = _checkerboard.CheckWinner();
             if (winnerStone == StoneType.Null)
             {
-                var otherPlayerGuid = _players.Where(entry => entry.PlayerGuid != playerGuid).Select(entry => entry.PlayerGuid).First();
-                StartTurn(otherPlayerGuid);
+                var opponentPlayerGuid = _checkerboard.GetOpponentPlayerGuid(playerGuid);
+                StartTurn(opponentPlayerGuid);
             }
             else
             {
                 PlayerTurnStartedEvent -= OnPlayerTurnStartedEvent;
+                PlayerTurnFinishedEvent -= OnPlayerTurnFinishedEvent;
 
-                for (int i = 0; i < 19; i++)
-                    for (int j = 0; j < 19; j++)
-                        _board[i][j] = StoneType.Null;
+                var winnerGuid = _checkerboard.GetPlayerGuid(winnerStone);
+                _checkerboard.Clear();
 
-                var winner = _players.Where(entry => entry.StoneType == winnerStone).First();
                 GameFinishedEvent?.Invoke(this, new GameFinishedEventArgs
                 {
-                    WinnerGuid = winner.PlayerGuid,
-                    WinnerStone = winner.StoneType
+                    WinnerGuid = winnerGuid,
+                    WinnerStone = winnerStone
                 });
             }
         }
 
-        private async void OnPlayerTurnStartedEvent(object sender, PlayerTurnStartedEventArgs args)
+        private void OnPlayerTurnStartedEvent(object sender, PlayerTurnStartedEventArgs args)
         {
-            var player = _players[1];
-            if (player.PlayerGuid != args.PlayerGuid)
+            if (args.PlayerGuid != _dummyPlayer.GetPlayerGuid())
                 return;
 
-            var cts = new CancellationTokenSource();
-            _timer.Start(1, _timeLimit, () =>
-            {
-                cts?.Cancel();
-                cts?.Dispose();
-            });
+            _dummyPlayer.StartTurn();
+        }
 
-            var point = await _aiConsole.RequestStonePoint(player.StoneType, _board.Select(row => row.ToArray()).ToArray(), cts.Token);
-
-            _timer.Stop(1);
-            if (point == null)
+        private void OnPlayerTurnFinishedEvent(object sender, PlayerTurnFinishedEventArgs args)
+        {
+            if (args.PlayerGuid != _dummyPlayer.GetPlayerGuid())
                 return;
-            PutStone(_gameGuid, player.PlayerGuid, point.Item1, point.Item2);
-        }
 
-        private int CheckWinner(int[][] board)
-        {
-            int rows = board.Length;
-            if (rows == 0) return StoneType.Null;
-            int cols = board[0].Length;
-
-            for (int r = 0; r < rows; r++)
-            {
-                for (int c = 0; c < cols; c++)
-                {
-                    if (board[r][c] == 0) continue;
-
-                    int stone = board[r][c];
-
-                    if (CheckDirection(board, r, c, 0, 1, stone) || // Horizontal
-                        CheckDirection(board, r, c, 1, 0, stone) || // Vertical
-                        CheckDirection(board, r, c, 1, 1, stone) || // Right down
-                        CheckDirection(board, r, c, 1, -1, stone))  // Left down
-                    {
-                        return stone;
-                    }
-                }
-            }
-
-            return StoneType.Null;
-        }
-
-        private bool CheckDirection(int[][] board, int r, int c, int dr, int dc, int stone)
-        {
-            int count = 1;
-            int rows = board.Length;
-            int cols = board[0].Length;
-
-            for (int i = 1; i < 5; i++)
-            {
-                int nr = r + (dr * i);
-                int nc = c + (dc * i);
-
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && board[nr][nc] == stone)
-                {
-                    count++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-
-            return count == 5;
+            _dummyPlayer.HaltTurn();
         }
     }
 }
