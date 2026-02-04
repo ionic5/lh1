768ff3cc5a674112b241909bd872ca92dc2d7991
ionic5 <st.kaldalis@gmail.com>
2026-02-04 14:15:03 +0900

    MockGameSession 정리

--- LikeLion.LH1.Client/Assets/Scripts/Core/GameScene/MockGameSession.cs
+++ LikeLion.LH1.Client/Assets/Scripts/Core/GameScene/MockGameSession.cs
@@ -15,8 +15,180 @@ namespace LikeLion.LH1.Client.Core.GameScene
         private readonly Core.Timer _timer;
         private readonly float _timeLimit;
         private readonly Entity.Checkerboard _checkerboard;
-        private IPlayer _dummyPlayer;
         private readonly Func<IPlayer> _createDummyPlayer;
+
+        private IPlayer _dummyPlayer;
+        private SessionState _state;
+        private bool _isDestroyed;
+
+        private abstract class SessionState
+        {
+            public virtual void Enter() { }
+            public virtual void RequestGame(string playerGuid) { }
+            public virtual void PickStone(string gameGuid, string playerGuid, int stoneType) { }
+            public virtual void StartGame(string gameGuid, string playerGuid) { }
+            public virtual void PutStone(string gameGuid, string playerGuid, int column, int row) { }
+            public virtual void Exit() { }
+        }
+
+        private class WaitingState : SessionState
+        {
+            private readonly MockGameSession _session;
+
+            public WaitingState(MockGameSession session)
+            {
+                _session = session;
+            }
+
+            public override void RequestGame(string playerGuid)
+            {
+                _session._dummyPlayer = _session._createDummyPlayer();
+                _session._dummyPlayer.SetPlayerGuid(Guid.NewGuid().ToString());
+
+                var gameGuid = Guid.NewGuid().ToString();
+                _session._checkerboard.SetGameGuid(gameGuid);
+                _session._checkerboard.Setup();
+
+                _session.GameCreatedEvent?.Invoke(_session, new GameCreatedEventArgs { GameGuid = gameGuid });
+            }
+
+            public override void PickStone(string gameGuid, string playerGuid, int stoneType)
+            {
+                var dummyPlayer = _session._dummyPlayer;
+
+                _session._checkerboard.RegisterStoneOwner(playerGuid, stoneType);
+                var dummyPlayerStone = stoneType == StoneType.Black ? StoneType.White : StoneType.Black;
+                _session._checkerboard.RegisterStoneOwner(dummyPlayer.GetPlayerGuid(), dummyPlayerStone);
+
+                var stoneOwners = new List<StoneOwner>
+                {
+                    new StoneOwner { PlayerGuid = playerGuid, StoneType = stoneType },
+                    new StoneOwner { PlayerGuid = dummyPlayer.GetPlayerGuid(), StoneType = dummyPlayerStone }
+                };
+                _session.GamePreparedEvent?.Invoke(this, new GamePreparedEventArgs { StoneOwners = stoneOwners });
+            }
+
+            public override void StartGame(string gameGuid, string playerGuid)
+            {
+                var newState = new InGameState(_session);
+                _session.ChangeState(newState);
+                newState.StartGame(gameGuid, playerGuid);
+            }
+        }
+
+        private class InGameState : SessionState
+        {
+            private readonly MockGameSession _session;
+
+            public InGameState(MockGameSession session)
+            {
+                _session = session;
+            }
+
+            public override void Enter()
+            {
+                _session.PlayerTurnStartedEvent += OnPlayerTurnStartedEvent;
+                _session.PlayerTurnFinishedEvent += OnPlayerTurnFinishedEvent;
+            }
+
+            public override void StartGame(string gameGuid, string playerGuid)
+            {
+                var firstTurnPlayerGuid = _session._checkerboard.GetPlayerGuid(StoneType.Black);
+                StartTurn(firstTurnPlayerGuid);
+            }
+
+            private void StartTurn(string playerGuid)
+            {
+                _session.PlayerTurnStartedEvent?.Invoke(this, new PlayerTurnStartedEventArgs
+                {
+                    PlayerGuid = playerGuid,
+                    TimeLimit = _session._timeLimit
+                });
+
+                _session._timer.Start(0, _session._timeLimit, () =>
+                {
+                    TriggerPlayerTurnFinishedEvent(playerGuid, -1, -1, StoneType.Null);
+
+                    StartOpponentPlayerTurn(playerGuid);
+                });
+            }
+
+            private void StartOpponentPlayerTurn(string playerGuid)
+            {
+                var opponentPlayerGuid = _session._checkerboard.GetOpponentPlayerGuid(playerGuid);
+                StartTurn(opponentPlayerGuid);
+            }
+
+            public override void PutStone(string gameGuid, string playerGuid, int column, int row)
+            {
+                var stoneType = _session._checkerboard.GetStone(playerGuid);
+
+                _session._checkerboard.PutStone(column, row, stoneType);
+
+                _session._timer.Stop(0);
+
+                TriggerPlayerTurnFinishedEvent(playerGuid, column, row, stoneType);
+
+                var winnerStone = _session._checkerboard.CheckWinner();
+                if (winnerStone == StoneType.Null)
+                {
+                    StartOpponentPlayerTurn(playerGuid);
+                }
+                else
+                {
+                    _session.ChangeState(new WaitingState(_session));
+                    FinishGame(winnerStone);
+                }
+            }
+
+            private void TriggerPlayerTurnFinishedEvent(string playerGuid, int column, int row, int stoneType)
+            {
+                _session.PlayerTurnFinishedEvent?.Invoke(this, new PlayerTurnFinishedEventArgs
+                {
+                    PlayerGuid = playerGuid,
+                    StoneType = stoneType,
+                    Column = column,
+                    Row = row
+                });
+            }
+
+            private void FinishGame(int winnerStone)
+            {
+                var winnerGuid = _session._checkerboard.GetPlayerGuid(winnerStone);
+                _session._checkerboard.Clear();
+
+                _session.GameFinishedEvent?.Invoke(this, new GameFinishedEventArgs
+                {
+                    WinnerGuid = winnerGuid,
+                    WinnerStone = winnerStone
+                });
+            }
+
+            private void OnPlayerTurnStartedEvent(object sender, PlayerTurnStartedEventArgs args)
+            {
+                if (args.PlayerGuid != _session._dummyPlayer.GetPlayerGuid())
+                    return;
+
+                _session._dummyPlayer.StartTurn();
+            }
+
+            private void OnPlayerTurnFinishedEvent(object sender, PlayerTurnFinishedEventArgs args)
+            {
+                if (args.PlayerGuid != _session._dummyPlayer.GetPlayerGuid())
+                    return;
+
+                _session._dummyPlayer.HaltTurn();
+            }
+
+            public override void Exit()
+            {
+                _session.PlayerTurnStartedEvent -= OnPlayerTurnStartedEvent;
+                _session.PlayerTurnFinishedEvent -= OnPlayerTurnFinishedEvent;
+                _session._timer.Stop(0);
+                _session._dummyPlayer?.Destroy();
+                _session._dummyPlayer = null;
+            }
+        }
 
         public MockGameSession(Timer timer, Entity.Checkerboard checkerboard, Func<IPlayer> createDummyPlayer)
         {
@@ -24,136 +196,54 @@ namespace LikeLion.LH1.Client.Core.GameScene
             _timer = timer;
             _checkerboard = checkerboard;
             _createDummyPlayer = createDummyPlayer;
+            _isDestroyed = false;
+        }
+
+        private void ChangeState(SessionState newState)
+        {
+            _state?.Exit();
+            _state = newState;
+            _state.Enter();
         }
 
         public void RequestConnect()
         {
-            var playerGuid = Guid.NewGuid().ToString();
+            ChangeState(new WaitingState(this));
 
+            var playerGuid = Guid.NewGuid().ToString();
             ConnectedEvent?.Invoke(this, new ConnectedEventArgs { PlayerGuid = playerGuid });
         }
 
         public void RequestGame(string playerGuid)
         {
-            var dummyPlayerGuid = Guid.NewGuid().ToString();
-
-            _dummyPlayer = _createDummyPlayer();
-            _dummyPlayer.SetPlayerGuid(dummyPlayerGuid);
-
-            var gameGuid = Guid.NewGuid().ToString();
-            _checkerboard.SetGameGuid(gameGuid);
-            _checkerboard.Setup();
-
-            GameCreatedEvent?.Invoke(this, new GameCreatedEventArgs { GameGuid = gameGuid });
+            _state?.RequestGame(playerGuid);
         }
 
         public void PickStone(string gameGuid, string playerGuid, int stoneType)
         {
-            _checkerboard.RegisterStoneOwner(playerGuid, stoneType);
-
-            var dummyPlayerStoneType = StoneType.Black == stoneType ? StoneType.White : StoneType.Black;
-            _checkerboard.RegisterStoneOwner(_dummyPlayer.GetPlayerGuid(), dummyPlayerStoneType);
-
-            var stoneOwners = new List<StoneOwner>
-            {
-                new StoneOwner { PlayerGuid = playerGuid, StoneType = stoneType },
-                new StoneOwner { PlayerGuid = _dummyPlayer.GetPlayerGuid(), StoneType = dummyPlayerStoneType }
-            };
-            GamePreparedEvent?.Invoke(this, new GamePreparedEventArgs { StoneOwners = stoneOwners });
+            _state?.PickStone(gameGuid, playerGuid, stoneType);
         }
 
         public void StartGame(string gameGuid, string playerGuid)
         {
-            PlayerTurnStartedEvent += OnPlayerTurnStartedEvent;
-            PlayerTurnFinishedEvent += OnPlayerTurnFinishedEvent;
-
-            var firstTurnPlayerGuid = _checkerboard.GetPlayerGuid(StoneType.Black);
-            StartTurn(firstTurnPlayerGuid);
-        }
-
-        private void StartTurn(string playerGuid)
-        {
-            PlayerTurnStartedEvent?.Invoke(this, new PlayerTurnStartedEventArgs
-            {
-                PlayerGuid = playerGuid,
-                TimeLimit = _timeLimit
-            });
-
-            _timer.Start(0, _timeLimit, () =>
-            {
-                PlayerTurnFinishedEvent?.Invoke(this, new PlayerTurnFinishedEventArgs
-                {
-                    PlayerGuid = playerGuid,
-                    StoneType = StoneType.Null,
-                    Column = -1,
-                    Row = -1
-                });
-
-                var opponentPlayerGuid = _checkerboard.GetOpponentPlayerGuid(playerGuid);
-                StartTurn(opponentPlayerGuid);
-            });
+            _state?.StartGame(gameGuid, playerGuid);
         }
 
         public void PutStone(string gameGuid, string playerGuid, int column, int row)
         {
-            var stoneType = _checkerboard.GetStone(playerGuid);
-
-            _checkerboard.PutStone(column, row, stoneType);
-
-            _timer.Stop(0);
-
-            PlayerTurnFinishedEvent?.Invoke(this, new PlayerTurnFinishedEventArgs
-            {
-                PlayerGuid = playerGuid,
-                StoneType = stoneType,
-                Column = column,
-                Row = row
-            });
-
-            var winnerStone = _checkerboard.CheckWinner();
-            if (winnerStone == StoneType.Null)
-            {
-                var opponentPlayerGuid = _checkerboard.GetOpponentPlayerGuid(playerGuid);
-                StartTurn(opponentPlayerGuid);
-            }
-            else
-            {
-                PlayerTurnStartedEvent -= OnPlayerTurnStartedEvent;
-                PlayerTurnFinishedEvent -= OnPlayerTurnFinishedEvent;
-
-                _dummyPlayer.Destroy();
-                _dummyPlayer = null;
-
-                var winnerGuid = _checkerboard.GetPlayerGuid(winnerStone);
-                _checkerboard.Clear();
-
-                GameFinishedEvent?.Invoke(this, new GameFinishedEventArgs
-                {
-                    WinnerGuid = winnerGuid,
-                    WinnerStone = winnerStone
-                });
-            }
-        }
-
-        private void OnPlayerTurnStartedEvent(object sender, PlayerTurnStartedEventArgs args)
-        {
-            if (args.PlayerGuid != _dummyPlayer.GetPlayerGuid())
-                return;
-
-            _dummyPlayer.StartTurn();
-        }
-
-        private void OnPlayerTurnFinishedEvent(object sender, PlayerTurnFinishedEventArgs args)
-        {
-            if (args.PlayerGuid != _dummyPlayer.GetPlayerGuid())
-                return;
-
-            _dummyPlayer.HaltTurn();
+            _state?.PutStone(gameGuid, playerGuid, column, row);
         }
 
         public void Destroy()
         {
-            throw new NotImplementedException();
+            if (_isDestroyed)
+                return;
+            _isDestroyed = true;
+
+            _state?.Exit();
+            _state = null;
+
+            _timer.Destroy();
         }
     }
 }
